fff03fccdd3e46ec3db4f2eed137ac6384fc5f64
Kwangsub Ahn <ejekk@hotmail.com>
2015-10-25 01:03:44 +0900

    Add prototype pages

--- app.js
+++ app.js
@@ -33,3 +33,47 @@ app.get('/users', user.list);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+app.get('/RegisterHostFamily', function(req, res){
+	  res.render('RegisterHostFamily', {
+	    title: 'RegisterHostFamily'
+	  });
+	});
+
+app.get('/home', function(req, res){
+	  res.render('home', {
+	    title: 'home'
+	  });
+	});
+
+app.get('/AssignMentor', function(req, res){
+	  res.render('AssignMentor', {
+	    title: 'AssignMentor'
+	  });
+	});
+
+app.get('/ManageEvents', function(req, res){
+	  res.render('ManageEvents', {
+	    title: 'ManageEvents'
+	  });
+	});
+
+app.get('/ViewStaff', function(req, res){
+	  res.render('ViewStaff', {
+	    title: 'ViewStaff'
+	  });
+	});
+
+app.get('/RegisterMentor', function(req, res){
+	  res.render('RegisterMentor', {
+	    title: 'RegisterMentor'
+	  });
+	});
+
+app.get('/ChooseHostFamily', function(req, res){
+	  res.render('ChooseHostFamily', {
+	    title: 'ChooseHostFamily'
+	  });
+	});
