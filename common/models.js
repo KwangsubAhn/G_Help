6785c12233d67aece8e1c65ce5ebd4231ab0e280
fjkaoru <jiachenxiao@hotmail.com>
2015-11-15 20:39:24 -0330

    Change the DataStructure

--- common/models.js
+++ common/models.js
@@ -5,4 +5,17 @@ module.exports = {
         cAddress: { type: String, required: true },
 		cPhoneNum: { type: String, required: true }
     },
+    Student: {
+        cStuID: { type: String, required: true },
+        cStuName: { type: String, required: true },
+		cEnrolYear: { type: Number, required: true },
+		cCourseNum: { type: Array },
+		cIsMentor: { type: Boolean, default: false },
+		cPairedMentorStuNum: { type: String },
+    },
+    Course: {
+        cCourseNum: { type: Number, required: true },
+        cCourseName: { type: String, required: true },
+		cCourseDescription: { type: String, required: true }
+    },
 };
